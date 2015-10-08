96687de7cb2f18827bc88a01424f3bc355886b4f
HilsonF <hilson_francis@Hilsons-MacBook-Pro.local>
2015-10-08 02:44:23 -0400

    made external routes file and instaled mongoose in project

--- app.js
+++ app.js
@@ -6,7 +6,7 @@
 var express 	= require('express'),
  	http 		= require('http'),
  	exphbs 		= require('express-handlebars')
-	path = require('path');
+	path 		= require('path');
 
 var app = express();
 
@@ -15,6 +15,8 @@ app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'handlebars');
+
+// static file folders
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'bower_components')));
 
@@ -23,9 +25,8 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-app.get('/', function (req, res) {
-  res.render('homepage');
-});
+//Routes
+require('./routes/routes')(app);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
---
+++ config/db.js
@@ -0,0 +1,14 @@
+
+var mongoose = require( 'mongoose' );
+ 
+var unString = 'mongodb://<dbuser>:<dbpassword>@ds029814.mongolab.com:29814/heroku_l631wd40' || process.env.MONGOLAB_URI || process.env.MONGOLAB_URL || 'mongodb://localhost:27017/<sonthing>';
+
+//MONGOOSE CONECTION
+mongoose.connect(unString, function(err, res){
+	if(err){
+		console.log('ERROR CONECTING TO DATABASE');
+	}else{
+		console.log('CONNECTED TO DATABASE');
+	}
+});
+
