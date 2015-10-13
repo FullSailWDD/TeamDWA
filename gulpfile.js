74a6409cec9a9d8dd505b874bda16774ae812c8a
KaseKreative <KaseKreative@fullsail.edu>
2015-10-13 01:30:39 -0400

    gulp

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+var gulp = require('gulp'),
+	child_process = require('child_process'),
+	jeet = require('jeet'),
+	nodemon = require('gulp-nodemon'),
+	stylus = require('gulp-stylus'),
+	connect = require ('gulp-connect');
+
+
+
+	gulp.task('dev', function(){
+		nodemon({
+			script: 'app.js',
+			ext: 'js handlebars'
+		})
+	})
+
+	gulp.task('mongod', function(){
+		child_process.exec('mongo', function(err, stdout, stderr){
+			console.log(stdout);		
+		})
+	})
+
+	gulp.task('all', ['mongod', 'dev']);
