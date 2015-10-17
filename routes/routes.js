a8557dbb1976c6a549874fc3d3343b7e77073a8c
KaseKreative <KaseKreative@fullsail.edu>
2015-10-17 00:36:28 -0400

    deploying

--- routes/routes.js
+++ routes/routes.js
@@ -19,7 +19,7 @@
 // Login
         app.post('/login', passport.authenticate('local-login', {
             successRedirect : '/dashboard', // redirect to the secure profile section
-            failureRedirect : '/login', // redirect back to the signup page if there is an error
+            failureRedirect : '/', // redirect back to the signup page if there is an error
             failureFlash : true // allow flash messages
 
         }));
