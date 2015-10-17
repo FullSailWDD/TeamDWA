358a5a270dee7ba870c101606bac4c78a4bb23bd
KaseKreative <KaseKreative@fullsail.edu>
2015-10-16 22:53:57 -0400

    working on pushing to heroku

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 var mongoose = require( 'mongoose' );
 
-var unString = 'mongodb://Tebo:rootAdmin@ds039674.mongolab.com:39674/heroku_0tb5bbjg' || process.env.MONGOLAB_URI || process.env.MONGOLAB_URL || 'mongodb://localhost:27017/dform';//MONGOOSE CONECTION
+var unString = 'mongodb://Tebo:rootAdmin@ds039674.mongolab.com:39674/heroku_0tb5bbjg' || process.env.MONGOLAB_URI || process.env.MONGOLAB_URL;//MONGOOSE CONECTION
 mongoose.connect(unString, function(err, res){
     if(err){
         console.log('ERROR CONECTING TO DATABASE');
